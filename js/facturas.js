42a7ea0b7e3e8956d9eaee8b0377d9afbf4c0e2e
git-abadano <abadano05@gmail.com>
2022-09-08 11:36:34 -0300

    1er entrega del proyecto final, se agrega funciones de orden superior y html

--- js/facturas.js
+++ js/facturas.js
@@ -1,7 +1,9 @@
 let registrosFactura = [];
+let colFacturas = [];
 let nroCliente = 0;
 let fechaVto = "";
 let salir = false;
+let numeroFactura = 100000;
 
 // Clase Factura, representa una factura emitida a un cliente con el detalle de compra
 // atributos: tipo: indica si es contado o credito
@@ -65,6 +67,98 @@ class Factura {
     totalMasIVA(){
         return this.getTotal()*1.22;
     }
+    imprimirFactura(){
+        let str = "";
+        fechaEmision = Date.now();
+        let fechaHoy= new Date(fechaEmision);
+        fechaVto = new Date(2022,9,4);
+        const nroFact = this.getNro();
+        const tipoFact = this.getTipo();
+        const nroCli = this.getNroCliente();
+        const registros = this.getRegistrosImprimir();
+        const totalFact = roundToTwo(this.getTotal());
+        const totalFactMasIVA = roundToTwo(this.totalMasIVA());
+
+        str = "F A C T U R A    I N G R E S A D A:" + "\n" + "\n" +
+            "Tipo de factura: " + tipoFact + "\n" +
+            "Fecha factura: " + fechaHoy.toLocaleDateString() + "\n" + 
+            "Numero de cliente: " + nroCli + "\n" +
+            "Numero de factura: " + nroFact + "\n" + "\n" + 
+            "Detalle: " + "\n" + registros + "\n" +
+            "Total: $ " + totalFact + "\n" + 
+            "Total mas IVA $ " + totalFactMasIVA;
+        
+        return str;
+    
+    }
+}
+
+// Clase Facturas, representa una colección de facturas emitidas 
+// atributos: colFacturas: array de facturas
+//
+// metodos: getColFacturas(): devuelve el array con las facturas cargadas
+//          agregarFactura(factura): agrega un objeto del tipo factura a la colección de facturas 
+//                                   recibe un objeto del tipo factura
+//          estaVacio(): devuelve true si la colección está vacia
+//          eliminarFactura(nroFactura): elimina una factura de la colección, recibe un numero de factura 
+//          mostrarFacturas(): devuelve un string con los datos de las facturas de la colección para ser mostrados 
+//          mostrarNrosFacturas(): devuelve un string con los tipos de factura y los numeros de factura para ser 
+//                                 mostrados
+//          buscarFactura(nroFactura): devuelve un objeto del tipo factura, recibe un numero de factura para la busqueda
+class Facturas{
+    constructor(colFacturas){
+        this.colFacturas = colFacturas
+    }
+    getColFacturas(){
+        return this.colFacturas;
+    }
+    agregarFactura(factura){
+        colFacturas.push(factura);
+    }
+    estaVacio(){
+        let vacio = false;
+        if(this.getColFacturas().length === 0){
+            vacio = true;
+        }
+        return vacio;
+    }
+    eliminarFactura(nroFactura){
+        colFacturas = this.getColFacturas();
+        const indices = colFacturas.map((item) => {
+            return item.getNro();
+        })
+        const indice = indices.indexOf(nroFactura);
+        if (indice != -1){
+            this.colFacturas.splice(indice,1);
+        }
+        return indice;
+    }
+    mostrarFacturas(){
+        const colFacturas = this.getColFacturas();
+        let str = "";
+        colFacturas.forEach((item) => {
+            str = str + "Tipo factura: " + item.getTipo() + 
+                        " Numero factura: " + item.getNro() + 
+                        " Numero Cliente: " + item.getNroCliente() + 
+                        " Fecha vencimiento: " + item.getVto() + "\n" + 
+                        " Detalle: " + "\n" + item.getRegistrosImprimir() + "\n" + "\n"
+        })
+        return str
+    }
+    mostrarNrosFacturas(){
+        const colFacturas = this.getColFacturas();
+        let str = "";
+        colFacturas.forEach((item) => {
+            str = str + "Tipo factura: " + item.getTipo() + 
+                        " Numero factura: " + item.getNro() + "\n";
+        })
+        return str
+    }
+    buscarFactura(nroFactura){
+        colFacturas = this.getColFacturas();
+        const Factura = colFacturas.find((factura) => factura.nro == nroFactura);
+        return Factura;
+    }
 }
 
 // Clase RegistrosFactura, representa los registros o detalle de una factura emitida a un cliente
@@ -100,6 +194,7 @@ class RegistroFactura {
     getTotalRegistro(){
         return this.calculoTotalRegistro();
     }
+    
 }
 
 // Función para validar si un valor es null
@@ -136,6 +231,13 @@ const validarDato = (str) =>{
 
     return dato;
 }
+const vaciarArray = (array) =>{
+    
+    while(array.length !=0){
+        array.pop();
+    }
+}
+
 // Función para realizar la carga de los registros o detalle de la factura, por cada uno de los datos a  
 // ingresar, se controla que sean datos numericos en caso de no ingresar un numero, se continua pidiendo el valor
 // con el botón cancelar se sale de la ventana y del menu.
@@ -148,6 +250,10 @@ const cargaRegistrosFactura = () =>{
     let cantidad = 0;
     let precioUnitario = 0;
     let continuar = 0;
+
+    while(registrosFactura.length !=0){
+        registrosFactura.pop();
+    }
     
     do{
         (!salir) ? codigo = parseInt(validarDato("Ingrese codigo de producto: ")) : "";
@@ -162,34 +268,106 @@ const cargaRegistrosFactura = () =>{
     }while ((continuar === "1") & (!salir))
 }
 
-nroCliente = validarDato("Ingrese numero de Cliente: ");
-cargaRegistrosFactura();
+let ColFacturas = new Facturas(colFacturas); 
+let otraFactura = "1";
+// bucle para poder cargar facturas a la colección de facturas, con el numero 1 se sigue cargando facturas y con 
+// cualquier otra tecla se sale
+while ((otraFactura === "1") & (!salir)){
 
-// En caso de no tener la solicitud de salir de programa se crean otras variables para luego dar de alta la factura
-// se muestra por pantalla los datos cargados
-if (!salir)  {
-    fechaEmision = Date.now();
-    let fechaHoy= new Date(fechaEmision);
-    fechaVto = new Date(2022,9,4);
-    let nuevaFactura = new Factura("CONTADO",100001,nroCliente,registrosFactura,fechaVto);
+    nroCliente = validarDato("Ingrese numero de Cliente: ");
+    cargaRegistrosFactura();
 
-    nroFact = nuevaFactura.getNro();
-    tipoFact = nuevaFactura.getTipo();
-    nroCli = nuevaFactura.getNroCliente();
-    registros = nuevaFactura.getRegistrosImprimir();
-    totalFact = roundToTwo(nuevaFactura.getTotal());
-    totalFactMasIVA = roundToTwo(nuevaFactura.totalMasIVA());
+    if (!salir)  {
+        fechaEmision = Date.now();
+        let fechaHoy= new Date(fechaEmision);
+        fechaVto = new Date(2022,9,4);
+        let nuevaFactura = new Factura("CONTADO",numeroFactura,nroCliente,registrosFactura,fechaVto);
+        ColFacturas.agregarFactura(nuevaFactura);
 
-    alert("F A C T U R A    I N G R E S A D A:" + "\n" + "\n" +
-        "Tipo de factura: " + tipoFact + "\n" +
-        "Fecha factura: " + fechaHoy.toLocaleDateString() + "\n" + 
-        "Numero de cliente: " + nroCli + "\n" +
-        "Numero de factura: " + nroFact + "\n" + "\n" + 
-        "Detalle: " + "\n" + registros + "\n" +
-        "Total: $ " + totalFact + "\n" + 
-        "Total mas IVA $ " + totalFactMasIVA);
+        alert(nuevaFactura.imprimirFactura());
+
+        otraFactura = prompt("¿Desea ingresar otra Factura? /// (1 confirma / cualquier otra tecla termina)");
+        numeroFactura++;
+    }
+    else
+    {
+        alert("Exit");
+    }
 }
-else
-{
-    alert("Exit");
+
+// variables utilizadas para la busqueda y eliminación de facturas
+let nroFactura;
+let resultado_factura;
+let otraOper="1";
+
+// Iteracción para poder borrar facturas, se muestra los numeros de facturas cargados en la colección, se solicita
+// numero de factura a borrar, con el 1 se continua borrando otras facturas con cualauier otra tecla se sale
+(!salir) ? alert("ELIMINACION DE FACTURAS"):"";
+while ((otraOper === "1") & (!salir)){
+    
+    if (ColFacturas.estaVacio())
+    {
+        alert("No existen facturas ingresadas en el sistema");
+        salir = true;
+    }
+    else
+    {
+        alert("Numeros de Facturas ingresados:" + "\n" + "\n" + ColFacturas.mostrarNrosFacturas());
+        nroFactura = parseInt(validarDato("Ingrese nro de factrua a borrar: "));
+    }
+
+    resultado_factura = ColFacturas.buscarFactura(nroFactura);
+    if (!salir)
+    {
+        if (resultado_factura === undefined){
+            alert("No existe nro de factura !!!!!!")
+        }
+        else
+        {
+            ColFacturas.eliminarFactura(nroFactura);
+            alert("Detalle de Factura eliminada: " +"\n"+"\n" +resultado_factura.imprimirFactura());
+        }
+        otraOper = prompt("¿Desea realizar otra eliminación: ? /// (1 confirma / cualquier otra tecla termina)");
+    }
+    else
+    {
+        alert("Exit");
+    }
+}
+
+otraOper = "1";
+// Iteracción para poder realizar busquede de datos de facturas, se solicita nro de factura, se muestran los 
+// numeros de facturas disponibles y tipo de facturas disponibles, con el 1 se sigue realizando la busqueda 
+// de nuevas facturas con cualquier otra tecla se sale 
+(!salir) ? alert("BUSQUEDA DE FACTURAS"):"";
+while ((otraOper === "1") & (!salir)){
+    
+    if (ColFacturas.estaVacio())
+    {
+        alert("No existen facturas ingresadas en el sistema");
+        salir = true;
+    }
+    else
+    {
+        alert("Numeros de Facturas ingresados:" + "\n" + "\n" + ColFacturas.mostrarNrosFacturas());
+
+    }
+        
+    nroFactura = parseInt(validarDato("Ingrese nro de factura a buscar: "));
+    if(!salir){
+        resultado_factura = ColFacturas.buscarFactura(nroFactura);
+
+        if (resultado_factura === undefined){
+            alert("No existe nro de factura !!!!!!")
+        }
+        else
+        {
+            alert("Factura encontrada: " + resultado_factura.imprimirFactura());
+        }   
+        otraOper = prompt("¿Desea realizar otra busqueda: ? /// (1 confirma / cualquier otra tecla termina)");
+    }
+    else
+    {
+        alert("Exit");
+    }
 }
